1127a62a1c9eee0f3e8d7d5c2caf70c2650e4457
Luiz Gustavo <lalmeida727.lg@gmail.com>
2022-06-27 15:36:25 -0300

    Add files via upload

---
+++ Primeiro Código em C.c
@@ -0,0 +1,29 @@
+#include <stdio.h> // Inclui a biblioteca stdio.h ao nosso código.
+// * stdio = standard input-output *
+
+int main () { // Principal função do código .
+
+// Declaração de variáveis: representação de valores variáveis;
+// Declaração de constantes: representação de valores constantes;
+
+// COMO DECLARAR UMA VARIÁVEL?
+// Sintaxe:
+// tipo nome;
+                                                                    // Dentro da função main temos um bloco de comandos na linguagem C.
+// TIPOS DE VARIÁVEIS:
+// char -> caracter
+// int -> inteiro simples
+// long int -> inteiro longo
+// float -> real de precisão simples
+// double -> real de precisão dupla
+
+// Exemplo:
+
+int a, b
+double x, y;
+
+return 0;     // "0" informando ao sistema operacional que o programa foi executado corretamente.
+}
+
+// MODIFICADORES:
+// short, unsigned, long
